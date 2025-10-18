a3fd55ed865845a4e7bca2431fd6c9c2a8cb4c97
LaurenMBell <bell.laurenmar@gmail.com>
2025-10-18 00:23:33 -0700

    added baseline

--- network_property_table.sh
+++ network_property_table.sh
@@ -3,3 +3,14 @@ python data_preparation.py
 echo "done!"
 
 
+echo "Calculating metabolite correlations..."
+python metabolite_correlations.py
+echo "done!"
+
+echo "making the FDR and meta-analysis table..."
+python fdr_table.py
+echo "done!"
+
+echo "making the network properties table..."
+python network_properties_table.py
+echo "done with everything!"
